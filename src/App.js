f3d32d145069e48680cd5ea07c1087ce43da2d4e
Garrett Stewart <garrettbstewart@gmail.com>
2024-01-12 13:15:00 -0500

    TicketEdit and Nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
 import Dashboard from "./pages/Dashboard";
 import TicketPage from "./pages/TicketPage";
 
@@ -6,9 +7,11 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/ticket" element={<TicketPage />} />
+          <Route path="/ticket/:id" element={<TicketPage editMode={true} />} />
         </Routes>
       </BrowserRouter>
     </div>
