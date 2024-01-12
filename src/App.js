588fbfced70a1cdcd9607573d9bcf71be3426074
Garrett Stewart <garrettbstewart@gmail.com>
2024-01-12 13:12:39 -0500

    TicketPage Route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import TicketPage from "./pages/TicketPage";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/ticket" element={<TicketPage />} />
         </Routes>
       </BrowserRouter>
     </div>
