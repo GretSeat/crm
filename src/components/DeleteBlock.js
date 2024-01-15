f4c5e31d44837ab47216e7e92a1ec61cb60ac15e
Garrett Stewart <garrettbstewart@gmail.com>
2024-01-15 12:35:01 -0500

    Delete Block

--- src/components/DeleteBlock.js
+++ src/components/DeleteBlock.js
@@ -1,5 +1,14 @@
 function DeleteBlock() {
-  return <div>DeleteBlock</div>;
+  const deleteTicket = () => {
+    console.log("DELETED");
+  };
+  return (
+    <div className="delete-block">
+      <div className="delete-icon" onClick={deleteTicket}>
+        🗙
+      </div>
+    </div>
+  );
 }
 
 export default DeleteBlock;
