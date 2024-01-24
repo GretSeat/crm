7487b135ec0e311ca7c9ad873b698ea18e712fdc
Garrett Stewart <garrettbstewart@gmail.com>
2024-01-23 19:06:16 -0500

    backend set up

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Server is Running");
+const PORT = 8000;
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const axios = require("axios");
+
+const app = express();
+
+const url = app.listen(PORT, () =>
+  console.log(`Server Running on PORT: ${PORT}`)
+);
